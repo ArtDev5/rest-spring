9835910ed4aec03a064b31b2b61e1a5ad28b5336
Artur Silva <artur.antonio02@gmail.com>
2021-05-26 17:03:56 -0300

    Iniciando Spring Boot

---
+++ estudando-rest-spring/src/test/java/ApiRestSpring/estudandorestspring/EstudandoRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiRestSpring.estudandorestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandoRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
