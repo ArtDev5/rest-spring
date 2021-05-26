1540d7c0e5d237a7701540e22554745dd2037b66
Artur Silva <artur.antonio02@gmail.com>
2021-05-26 18:27:11 -0300

    Atualizando o Controller de Filmes
    Criação da instância do "Repository" de filmes e o método GET

--- estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/controller/FilmesController.java
+++ estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/controller/FilmesController.java
@@ -2,16 +2,23 @@ package ApiRestSpring.estudandorestspring.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ApiRestSpring.estudandorestspring.model.Filmes;
+import ApiRestSpring.estudandorestspring.repository.FilmesRepository;
 
 @RestController
 @RequestMapping("/filmes")
 public class FilmesController {
 	
+	@Autowired
+	private FilmesRepository filmesRepository;
+	
+	@GetMapping
 	public List<Filmes> listar(){
-		
+		return filmesRepository.findAll();
 	}
 }
