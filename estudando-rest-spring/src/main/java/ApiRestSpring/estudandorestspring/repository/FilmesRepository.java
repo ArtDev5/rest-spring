e57e5c66839111ec1fe390e3e18f249e19f81620
Artur Silva <artur.antonio02@gmail.com>
2021-05-27 01:00:29 -0300

    Criação do método que puxa os valores null da coluna avaliação

--- estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/repository/FilmesRepository.java
+++ estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/repository/FilmesRepository.java
@@ -1,11 +1,15 @@
 package ApiRestSpring.estudandorestspring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ApiRestSpring.estudandorestspring.model.Filmes;
 
 @Repository
 public interface FilmesRepository extends JpaRepository<Filmes, Long>{
-
+	@Query(value = "SELECT * FROM Filmes WHERE avaliacao is null", nativeQuery = true)
+	List<Filmes> getRecomendacao();
 }
