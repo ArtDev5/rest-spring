5ce486b548beefd01a122360d54164f57bdcf21e
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-05-28 23:45:46 -0300

    Criação do método Put, para avaliar os filmes
    Consegui fazer algo que não havia conseguido usando Nodejs, que era passar apenas o parâmetro de avaliação junto com o id e não deixar os outros atributos vazios.

--- estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/controller/FilmesController.java
+++ estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/controller/FilmesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -58,5 +59,17 @@ public class FilmesController {
 		filmesRepository.deleteById(id);
 	}
 	
-	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	Filmes avaliarFilme(@RequestBody Filmes filmeAvaliado, @PathVariable Long id) {
+		return filmesRepository.findById(id)
+				.map(avaliacao -> {
+					avaliacao.setAvaliacao(filmeAvaliado.getAvaliacao());
+					return filmesRepository.save(avaliacao);
+				})
+				.orElseGet(() -> {
+					filmeAvaliado.setId(id);
+					return filmesRepository.save(filmeAvaliado);
+				});
+	}
 }
