651a1b397140de56041ed8fdf800853fbc2d9d2e
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-05-28 23:02:13 -0300

    Criação do método Delete

--- estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/controller/FilmesController.java
+++ estudando-rest-spring/src/main/java/ApiRestSpring/estudandorestspring/controller/FilmesController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,4 +51,12 @@ public class FilmesController {
 		return response;
 		
 	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public void deletarFilme(@PathVariable Long id) {
+		filmesRepository.deleteById(id);
+	}
+	
+	
 }
